59bbcf954f4b25ddb83ecd7ffd4f0a4c08970991
Luca Rossi <lucarossi147@gmail.com>
2026-07-23 10:57:23 +0200

    fix: added E192 as return in devices

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,7 @@
 // Minimum versions;
-pub const PATCH_384: u32 = 15;
+pub const PATCH_384_VERSION: u32 = 15;
+pub const E192_VERSION: u32 = 13;
+
 pub const SUB_VERSION: u32 = 7;
 pub const FW_MAJOR: u32 = 0;
 pub const FW_MINOR: u32 = 0;
--- src/devices.rs
+++ src/devices.rs
@@ -1,6 +1,5 @@
 use e384_rust::device_info::DeviceVersionInfo;
-
-use crate::{constants::PATCH_384, devices::SupportedDevices::SyncroV1};
+use crate::{constants::{E192_VERSION, PATCH_384_VERSION}};
 
 pub mod e192;
 pub mod syncro;
@@ -13,7 +12,14 @@ pub enum SupportedDevices {
 impl SupportedDevices {
     pub fn from_device_version_info(di: &DeviceVersionInfo) -> Option<Self> {
         match di.device_version {
-            PATCH_384 => Some(SyncroV1),
+            PATCH_384_VERSION => match di.device_sub_version {
+                7 => Some(SupportedDevices::SyncroV1),
+                _ => None,
+            },
+            E192_VERSION => match di.device_sub_version {
+                7 => Some(SupportedDevices::E192),
+                _ => None,
+            },
             _ => None,
         }
     }
